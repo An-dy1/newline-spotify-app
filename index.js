a8bbf2e1a79dc70ba6e8885db547feb9ea5360b6
andie <andrea_sue2@icloud.com>
2022-11-22 15:01:29 -0600

    set up dev environment for spotify auth

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
+require('dotenv').config();
 const express = require('express')
 
 const app = express();
 const port = 8888;
+const CLIENT_ID = process.env.CLIENT_ID;
+const CLIENT_SECRET = process.env.CLIENT_SECRET;
+const REDIRECT_URI = process.env.REDIRECT_URI;
+
+console.log(process.env.CLIENT_ID);
 
 app.get('/', (_req, res) => {
     res.send('Hello World!')
