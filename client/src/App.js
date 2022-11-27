5295366933e82045dd80528fec158ec6cfb083c9
andie <andrea_sue2@icloud.com>
2022-11-27 14:15:23 -0600

    install react router and set up first two routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,13 @@
 import { useEffect, useState } from 'react';
 import { accessToken, logout, getUserProfile } from './spotify';
 import { catchErrors } from './utils';
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Link,
+  Routes,
+  Route,
+} from 'react-router-dom';
 import './App.css';
 
 function App() {
@@ -23,19 +30,44 @@ function App() {
     <div className='App'>
       <header className='App-header'>
         {token ? (
-          <>
-            <h1>Logged in</h1>
-            <button onClick={logout}>Logout</button>
+          <div>
+            <div>
+              <nav>
+                <ul>
+                  <li>
+                    <Link to='/top-artists'>Top Artists</Link>
+                    <Link to='/'>Home</Link>
+                  </li>
+                </ul>
+              </nav>
 
-            {profile && (
-              <div>
-                <h1>{profile.display_name}</h1>
-                {profile.images.length && profile.images[0].url && (
-                  <img src={profile.images[0].url} alt='User avatar' />
-                )}
-              </div>
-            )}
-          </>
+              <Routes>
+                <Route
+                  path='/top-artists'
+                  element={<h1>Top Artists</h1>}
+                ></Route>
+                <Route
+                  path='/'
+                  element={
+                    <>
+                      <button onClick={logout}>Log out</button>
+
+                      {profile && (
+                        <div>
+                          <h1>{profile.display_name}</h1>
+                          <p>{profile.followers.total} Followers</p>
+                          {profile.images.length > 0 &&
+                            profile.images[0].url && (
+                              <img src={profile.images[0].url} alt='profile' />
+                            )}
+                        </div>
+                      )}
+                    </>
+                  }
+                ></Route>
+              </Routes>
+            </div>
+          </div>
         ) : (
           <a className='App-link' href='http://localhost:8888/login'>
             Log in to Spotify
